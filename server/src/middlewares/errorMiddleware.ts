269b356ae097c45af8a04d8018d64496cebec1fb
Rikelme <grieprikelme@gmail.com>
2021-05-24 13:07:22 -0300

    api: create AppError

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,9 +1,14 @@
 import { ErrorRequestHandler } from 'express'
 import { ValidationError } from 'joi'
+import AppError from '../errors/AppError'
 
 const errorMiddleware: ErrorRequestHandler = (e, req, res, next) => {
   if (e instanceof ValidationError) {
     return res.status(400).json({ message: e.message || 'Validation error.' })
+  }
+
+  if (e instanceof AppError) {
+    return res.status(e.statusCode).json({ message: e.message })
   }
 
   console.error(e)
